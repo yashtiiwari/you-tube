64ac63fefe0a457d2dc875553b5f7535203d80a6
Yash Tiwari <yash6283061936@gmail.com>
2025-09-29 08:27:54 +0000

    3 lecture

---
+++ 01_/01_varriables.js
@@ -0,0 +1,6 @@
+var place="New Delhi"
+//var is old  practice because it gives probles relatedt to scope .
+let weight=23;
+const id=1;
+//const never let change the value once declared
+console.log(id,place,weight);
---
+++ 01_/02_datatypes.js
@@ -0,0 +1,13 @@
+//Primitive data types:
+let fullName="Yash Tiwari";
+let marks=50;
+let isSignedIn=false;
+let value=null;
+//null is intentional absence of value
+let fruitName;
+//here the fruit name is undefined
+//means a variable is declared but not assigned
+
+
+//Use of readablityTips 
+//  use strict for insta  
